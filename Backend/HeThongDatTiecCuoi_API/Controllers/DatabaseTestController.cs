0a5499d371bbe44e98881b901c622dbb9e860b91
miNHLog <leminhlong6805@gmail.com>
2026-09-11 17:07:21 +0700

    Ket noi database va them SanhTiec API

---
+++ Backend/HeThongDatTiecCuoi_API/Controllers/DatabaseTestController.cs
@@ -0,0 +1,37 @@
+﻿using HeThongDatTiecCuoi_API.Data;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+
+namespace HeThongDatTiecCuoi_API.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]")]
+    public class DatabaseTestController : ControllerBase
+    {
+        private readonly ApplicationDbContext _context;
+
+        public DatabaseTestController(ApplicationDbContext context)
+        {
+            _context = context;
+        }
+
+        [HttpGet]
+        public async Task<IActionResult> TestConnection()
+        {
+            bool canConnect = await _context.Database.CanConnectAsync();
+
+            if (canConnect)
+            {
+                return Ok(new
+                {
+                    message = "Kết nối SQL Server thành công"
+                });
+            }
+
+            return StatusCode(500, new
+            {
+                message = "Không thể kết nối SQL Server"
+            });
+        }
+    }
+}
--- Backend/HeThongDatTiecCuoi_API/Program.cs
+++ Backend/HeThongDatTiecCuoi_API/Program.cs
@@ -1,17 +1,26 @@
+using HeThongDatTiecCuoi_API.Data;
+using Microsoft.EntityFrameworkCore;
+
 var builder = WebApplication.CreateBuilder(args);
 
-// Add services to the container.
-
 builder.Services.AddControllers();
-// Learn more about configuring OpenAPI at https://aka.ms/aspnet/openapi
-builder.Services.AddOpenApi();
+
+builder.Services.AddDbContext<ApplicationDbContext>(options =>
+    options.UseSqlServer(
+        builder.Configuration.GetConnectionString("DefaultConnection")
+    )
+);
+
+// Swagger
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
 
 var app = builder.Build();
 
-// Configure the HTTP request pipeline.
 if (app.Environment.IsDevelopment())
 {
-    app.MapOpenApi();
+    app.UseSwagger();
+    app.UseSwaggerUI();
 }
 
 app.UseHttpsRedirection();
